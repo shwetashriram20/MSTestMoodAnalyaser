ca7b1966aab337f71cbb0525cbeeb1567fce1b48
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-01-17 06:32:05 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace UserRegisteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("User Registration Validating using Regular Expression!");
+            RegexSample regexSample = new RegexSample();
+            regexSample.Validating();
+
+        }
+    }
+}
