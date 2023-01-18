e6992adf16fdf8ca1423a05b124446a85a0e17c2
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-01-18 19:22:02 +0530

    Add files via upload

---
+++ MoodAnalyser.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserProb
+{
+    public class MoodAnalyser
+    {
+        string message;
+        public MoodAnalyser()
+        {
+            Console.WriteLine("Default constructor");
+        }
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+
+        public string AnalyzeMood()
+        {
+            try
+            {
+                if (message.Equals(string.Empty))
+                {
+                    throw new CustomException(CustomException.ExceptionType.EMPTY_EXCEPTION, "Mood should not be empty");
+                }
+                else if (message.ToLower().Contains("happy"))
+                {
+                    return "happy";
+                }
+                else
+                {
+                    return "sad";
+                }
+            }
+            catch (NullReferenceException)
+            {
+                throw new CustomException(CustomException.ExceptionType.NULL_EXCEPTION, "Mood should not be null");
+            }
+        }
+    }
+}
+
