f32e8bc18e3ea39550a8c4af3abdbdec2304f5a2
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-01-18 10:50:35 +0530

    Add files via upload

---
+++ MoodAnalyser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserProb
+{
+    public class MoodAnalyser
+    {
+        string message;
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+
+        public string AnalyzeMood()
+        {
+            if (message.ToLower().Contains("happy"))
+            {
+                return "happy";
+            }
+            else
+            {
+                return "sad";
+            }
+        }
+    }
+}
+   
